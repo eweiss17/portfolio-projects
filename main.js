cb5949cc26fe667d49699482a5764281018f9248
Eric Weiss <eweiss1995@gmail.com>
2018-06-14 00:16:38 -0400

    trying out new design to portfolio

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+      /* ----Materialize Functions ---- */
+      $('.button-collapse').sideNav();
+      $('.carousel.carousel-slider').carousel({
+          fullWidth: true
+      });
+
+  });
